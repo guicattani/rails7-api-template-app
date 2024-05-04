4768880fc98e5a8f4ee879cc1190721a6d3e053a
Guilherme Cattani <gcattani1@gmail.com>
2024-05-04 13:19:12 +0200

    Remove Coverband from routes
    Add alias command to pry command 'up'

--- .pryrc
+++ .pryrc
@@ -5,4 +5,5 @@ if defined?(PryByebug)
   Pry.commands.alias_command 's', 'step'
   Pry.commands.alias_command 'n', 'next'
   Pry.commands.alias_command 'f', 'finish'
+  Pry.commands.alias_command 'u', 'up'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  mount Coverband::Reporters::Web.new, at: 'prod_coverage' if Rails.env == 'production'
 end
